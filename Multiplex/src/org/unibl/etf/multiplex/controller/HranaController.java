ed71a6c77ea4ffaf0d151e443a0611f06d4d7cce
Ognjen1994 <ognjen.risti@gmail.com>
2017-08-31 00:51:44 +0200

    Hrana pice i racun
    Hrana pice i racun

---
+++ Multiplex/src/org/unibl/etf/multiplex/controller/HranaController.java
@@ -0,0 +1,244 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unibl.etf.multiplex.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.unibl.etf.model.adapter.ArtikalAdapter;
+import org.unibl.etf.model.domain.oo.ArtikalOO;
+import org.unibl.etf.model.domain.oo.RacunOO;
+import org.unibl.etf.model.domain.oo.StavkaOO;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ognjen
+ */
+public class HranaController implements Initializable {
+
+    @FXML
+    private ListView<StavkaOO> listaDodatihArtikala;
+    @FXML
+    private ListView<ArtikalOO> listaUkupnoArtikala;
+    @FXML
+    private Label ukupnoLBL;
+    @FXML
+    private TextField pretraziTXT;
+    @FXML
+    private TextField kolicinaTXT;
+    @FXML
+    private Button pretraziBTN;
+    @FXML
+    private Button dodajBTN;
+    @FXML
+    private Button nazadBTN;
+    @FXML
+    private Button obrisiBTN;
+    @FXML
+    private ObservableList<ArtikalOO> listaUkupnoArtikalaOBS;
+    ObservableList<StavkaOO> listaDodatihArtikalaOBS;
+
+    int kolicina = 0;
+    Double ukupnaCijena;
+    Double ukupnoZaPlatiti = 0.0;
+    String pretragaTXT;
+    ArrayList<ArtikalOO> ukupnoArtikli = new ArrayList<>();
+    RacunOO racun;
+
+    public HranaController(ObservableList listaDodatihArtikalaOBS, RacunOO racun) {
+        this.listaDodatihArtikalaOBS = listaDodatihArtikalaOBS;
+        this.racun = racun;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        dodajBTN.requestFocus();
+        setFieldForIntegersOnly(kolicinaTXT);
+        if (racun.getUkupnaCijena() == 0.0) {
+            ukupnoLBL.setVisible(false);
+        } else {
+            ukupnoLBL.setText("Ukupno za platiti: " + racun.getUkupnaCijena());
+        }
+
+        listaDodatihArtikala.setItems(listaDodatihArtikalaOBS);
+        listaDodatihArtikala.setCellFactory(lv -> new ListCell<StavkaOO>() {
+            @Override
+            public void updateItem(StavkaOO item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    this.setAlignment(Pos.CENTER);
+                    setText(item.getArtikal().getNaziv() + "                      x" + item.getKolicina() + "                   ukupno " + item.getArtikal().getCijena());
+                }
+            }
+        });
+
+        ukupnoArtikli.addAll(ArtikalAdapter.preuzmiPoTipu("Hrana"));
+        listaUkupnoArtikalaOBS = FXCollections.observableArrayList(ukupnoArtikli);
+        listaUkupnoArtikala.setItems(listaUkupnoArtikalaOBS);
+        listaUkupnoArtikala.setCellFactory(lv -> new ListCell<ArtikalOO>() {
+            @Override
+            public void updateItem(ArtikalOO item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    this.setAlignment(Pos.CENTER);
+                    setText(item.getNaziv() + " Stanje: " + item.getKolicinaNaStanju() + " Cijena: " + item.getCijena());
+
+                }
+            }
+        });
+
+        obrisiBTN.setOnAction(e -> obrisiBTN_Clicked());
+        dodajBTN.setOnAction(e -> dodajBTN_Clicked());
+        pretraziBTN.setOnAction(e -> pretraziBTN_Clicked());
+        nazadBTN.setOnAction(e -> nazadBTN_Clicked());
+        pretraziTXT.setOnKeyReleased(e -> pretraziBTN_Clicked());
+
+    }
+
+    private void dodajBTN_Clicked() {
+        if (!kolicinaTXT.getText().isEmpty()) {
+            kolicina = Integer.parseInt(kolicinaTXT.getText());
+        }
+        ArtikalOO artikal = listaUkupnoArtikala.getSelectionModel().getSelectedItem();
+        if (kolicina == 0 || artikal == null) {
+            neispravanOdabir("Niste odabrali stavku za dodavanje na racun ili niste unijeli kolicinu!");
+            return;
+        }
+
+        int pom = -1;
+        for (int i = 0; i < listaDodatihArtikalaOBS.size(); i++) {
+            if (listaDodatihArtikalaOBS.get(i).getArtikal().getArtikalId().equals(artikal.getArtikalId())) {
+                pom = i;
+            }
+        }
+        ukupnaCijena = kolicina * artikal.getCijena();
+        StavkaOO stavka = new StavkaOO(null, kolicina, ukupnaCijena, artikal);
+        if (pom != (-1)) {
+            listaDodatihArtikalaOBS.remove(pom);
+            racun.getStavke().remove(pom);
+            listaDodatihArtikalaOBS.add(pom, stavka);
+            racun.getStavke().add(pom, stavka);
+        } else {
+            listaDodatihArtikalaOBS.add(stavka);
+            racun.getStavke().add(stavka);
+        }
+
+        for (int i = 0; i < racun.getStavke().size(); i++) {
+            ukupnoZaPlatiti += racun.getStavke().get(i).getUkupnaCijena();
+        }
+        ukupnoLBL.setVisible(true);
+        ukupnoLBL.setText("Ukupno za platiti: " + ukupnoZaPlatiti.toString());
+        racun.setUkupnaCijena(ukupnoZaPlatiti);
+        kolicinaTXT.clear();
+        kolicina = 0;
+        ukupnoZaPlatiti = 0.0;
+
+    }
+
+    private void obrisiBTN_Clicked() {
+        StavkaOO stavkaSelektovana = listaDodatihArtikala.getSelectionModel().getSelectedItem();
+        if (stavkaSelektovana == null) {
+            neispravanOdabir("Niste odabrali stavku za brisanje!");
+            return;
+        }
+        listaDodatihArtikalaOBS.remove(stavkaSelektovana);
+        racun.getStavke().remove(stavkaSelektovana);
+        if (racun.getStavke().isEmpty()) {
+            ukupnoLBL.setVisible(false);
+            ukupnoZaPlatiti = 0.0;
+        } else {
+            for (int i = 0; i < racun.getStavke().size(); i++) {
+                ukupnoZaPlatiti += racun.getStavke().get(i).getUkupnaCijena();
+            }
+            ukupnoLBL.setText("Ukupno za platiti: " + ukupnoZaPlatiti.toString());
+        }
+        racun.setUkupnaCijena(ukupnoZaPlatiti);
+        ukupnoZaPlatiti = 0.0;
+    }
+
+    private void pretraziBTN_Clicked() {
+        if (!pretraziTXT.getText().isEmpty()) {
+            pretragaTXT = pretraziTXT.getText();
+            listaUkupnoArtikalaOBS.clear();
+            ArrayList<ArtikalOO> artikli = ArtikalAdapter.preuzmiPoTipu("Hrana");
+            for (ArtikalOO artikal : artikli) {
+                if (artikal.getNaziv().toLowerCase().startsWith(pretragaTXT.toLowerCase())) {
+                    listaUkupnoArtikalaOBS.add(artikal);
+                }
+            }
+        } else {
+            listaUkupnoArtikalaOBS.clear();
+            listaUkupnoArtikalaOBS.addAll(ArtikalAdapter.preuzmiPoTipu("Hrana"));
+        }
+
+    }
+
+    private void nazadBTN_Clicked() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/unibl/etf/multiplex/fxml/Racun.fxml"));
+
+        Parent root = null;
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(RacunController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Racun");
+        stage.setResizable(false);
+        ((Stage) nazadBTN.getScene().getWindow()).close();
+    }
+
+    private void setFieldForIntegersOnly(TextField input) {
+        input.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("^[1-9]+[0-9]*")) {
+                if (newValue.length() != 0) {
+                    input.setText(oldValue);
+                }
+            }
+        });
+    }
+
+    private void neispravanOdabir(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Informacioni dijalog");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+    public RacunOO preuzmiRacun() {
+        return racun;
+    }
+
+}
---
+++ Multiplex/src/org/unibl/etf/multiplex/controller/RacunController.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unibl.etf.multiplex.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import org.unibl.etf.model.adapter.RacunAdapter;
+import org.unibl.etf.model.domain.oo.RacunOO;
+import org.unibl.etf.model.domain.oo.StavkaOO;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ognjen
+ */
+public class RacunController implements Initializable {
+
+    @FXML
+    private Button hranaBTN;
+    @FXML
+    private Button piceBTN;
+    @FXML
+    private Button odustaniBTN;
+    @FXML
+    private Button stampajBTN;
+    @FXML
+    private Button obrisiBTN;
+    @FXML
+    private Label ukupnoLBL;
+    @FXML
+    private ListView<StavkaOO> listaDodatihArtikala;
+
+    Double ukupnoZaPlatiti = 0.0;
+    ObservableList<StavkaOO> listaDodatihArtikalaOBS = FXCollections.observableArrayList();
+    ArrayList<StavkaOO> stavke = new ArrayList<>();
+    RacunOO racun = new RacunOO(null, null, 0.0, stavke);
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        ukupnoLBL.setVisible(false);
+        listaDodatihArtikala.setItems(listaDodatihArtikalaOBS);
+        listaDodatihArtikala.setCellFactory(lv -> new ListCell<StavkaOO>() {
+            @Override
+            public void updateItem(StavkaOO item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    this.setAlignment(Pos.CENTER);
+                    setText(item.getArtikal().getNaziv() + "                      x" + item.getKolicina() + "                   ukupno " + item.getKolicina());
+                }
+            }
+        });
+        hranaBTN.setOnAction(e -> hranaBTN_Clicked());
+        piceBTN.setOnAction(e -> piceBTN_Clicked());
+        obrisiBTN.setOnAction(e -> obrisiBTN_Clicked());
+        odustaniBTN.setOnAction(e -> odustaniBTN_Clicked());
+        stampajBTN.setOnAction(e -> stampajBTN_Clicked());
+    }
+
+    private void hranaBTN_Clicked() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/unibl/etf/multiplex/fxml/Hrana.fxml"));
+        HranaController controller = new HranaController(listaDodatihArtikalaOBS, racun);
+        loader.setController(controller);
+        Parent root = null;
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(RacunController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Hrana");
+        stage.setResizable(false);
+        stage.showAndWait();
+        racun = controller.preuzmiRacun();
+        if (!racun.getStavke().isEmpty()) {
+            ukupnoLBL.setVisible(true);
+            ukupnoLBL.setText("Ukupno za platiti: " + racun.getUkupnaCijena());
+        } else {
+            ukupnoLBL.setVisible(false);
+        }
+    }
+
+    private void piceBTN_Clicked() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/unibl/etf/multiplex/fxml/Pice.fxml"));
+        PiceController controller = new PiceController(listaDodatihArtikalaOBS, racun);
+        loader.setController(controller);
+        Parent root = null;
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(RacunController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Pice");
+        stage.setResizable(false);
+        stage.showAndWait();
+        racun = controller.preuzmiRacun();
+        if (!racun.getStavke().isEmpty()) {
+            ukupnoLBL.setVisible(true);
+            ukupnoLBL.setText("Ukupno za platiti: " + racun.getUkupnaCijena());
+        } else {
+            ukupnoLBL.setVisible(false);
+        }
+    }
+
+    private void obrisiBTN_Clicked() {
+        StavkaOO stavkaSelektovana = listaDodatihArtikala.getSelectionModel().getSelectedItem();
+        if (stavkaSelektovana == null) {
+            neispravanOdabir("Niste odabrali stavku za brisanje!");
+            return;
+        }
+        listaDodatihArtikalaOBS.remove(stavkaSelektovana);
+        racun.getStavke().remove(stavkaSelektovana);
+        if (racun.getStavke().isEmpty()) {
+            ukupnoLBL.setVisible(false);
+            ukupnoZaPlatiti = 0.0;
+        } else {
+            for (int i = 0; i < racun.getStavke().size(); i++) {
+                ukupnoZaPlatiti += racun.getStavke().get(i).getUkupnaCijena();
+            }
+            ukupnoLBL.setText("Ukupno za platiti: " + ukupnoZaPlatiti.toString());
+        }
+        racun.setUkupnaCijena(ukupnoZaPlatiti);
+        ukupnoZaPlatiti = 0.0;
+    }
+
+    private void odustaniBTN_Clicked() {
+        ((Stage) odustaniBTN.getScene().getWindow()).close();
+    }
+
+    private void stampajBTN_Clicked() {
+        if (racun.getUkupnaCijena() != 0) {
+            racun.setDatumIzdavanja(new Date());
+            RacunAdapter.unesi(racun);
+        } else {
+            neispravanOdabir("Na racunu nema stavki!");
+        }
+
+    }
+
+    private void neispravanOdabir(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Informacioni dijalog");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+}
