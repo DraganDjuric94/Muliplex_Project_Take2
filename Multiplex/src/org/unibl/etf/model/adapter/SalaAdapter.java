c76da160d18debbdf4c01cadf8da0dfb5e06abc5
AleksandarV1 <vulebl94@gmail.com>
2017-08-31 17:07:36 +0200

    UPDATE: SalaAdapter
    Pored uocenih gresaka, napravljeno je da se vodi racuna o dimenzijama sale pri izmjeni.

--- Multiplex/src/org/unibl/etf/model/adapter/SalaAdapter.java
+++ Multiplex/src/org/unibl/etf/model/adapter/SalaAdapter.java
@@ -55,30 +55,55 @@ public class SalaAdapter {
     }
 
     public static void izmijeni(SalaOO salaOO) {
-        List<SjedisteOO> sjedisteOOList = SjedisteAdapter.preuzmiPoSalaId(salaOO.getSalaId());
-        for(SjedisteOO sjedisteOO: sjedisteOOList){
-            SjedisteAdapter.obrisi(sjedisteOO.getSjedisteId());
+        boolean promjenaDimenzijaSale = false;
+        
+        //provjeri da li je doslo do promjene
+        SalaOO salaDB = SalaAdapter.preuzmiPoId(salaOO.getSalaId());
+        if(
+                !salaDB.getBrojRedova().equals(salaOO.getBrojRedova()) ||
+                !salaDB.getBrojKolona().equals(salaOO.getBrojKolona())
+                ){
+            promjenaDimenzijaSale = true;
         }
         
-        for(SjedisteOO sjedisteOO: salaOO.getSjedista()){
-            SjedisteAdapter.unesi(salaOO.getSalaId(), sjedisteOO);
+        //Ako je doslo do promjene u dimenziji sale, obrisi stara sjedista i dodaj nova
+        if(promjenaDimenzijaSale){
+            List<SjedisteOO> sjedisteOOList = SjedisteAdapter.preuzmiPoSalaId(salaOO.getSalaId());
+            
+            for(SjedisteOO sjedisteOO: sjedisteOOList){
+                SjedisteAdapter.obrisi(sjedisteOO.getSjedisteId());
+            }
+            
+            //dodavanje brojRedova x brojKolona sjedista
+            for(int i = 0; i < salaOO.getBrojRedova(); i++){
+                for(int j = 0; j < salaOO.getBrojKolona(); j++){
+                    SjedisteAdapter.unesi(salaOO.getSalaId(), new SjedisteOO(null, (i + 1), (j + 1)));
+                }
+            }
+            
         }
         
-        salaDAO.update(konvertujUOV(salaOO));
-        
+        salaDAO.update(konvertujUOV(salaOO));    
     }
 
     public static void obrisi(Integer salaId) {
         List<ProjekcijaSala> projekcijaSalaList = projekcijaSalaDAO.selectBy(new ProjekcijaSala(null, salaId));
         
         for(ProjekcijaSala projekcijaSala: projekcijaSalaList){
+            ProjekcijaAdapter.obrisi(projekcijaSala.getProjekcijaId());
+        }
+        
+        for(ProjekcijaSala projekcijaSala: projekcijaSalaList){
             projekcijaSalaDAO.delete(salaId, projekcijaSala.getProjekcijaId());
         }
         
         SalaOO salaOO = preuzmiPoId(salaId);
+        
         for(SjedisteOO sjedisteOO: salaOO.getSjedista()){
-            SjedisteAdapter.obrisi(salaId);
+            SjedisteAdapter.obrisi(sjedisteOO.getSjedisteId());
         }
+        
+        salaDAO.delete(salaId);
         
         
     }
